8b9aa22a296d0458b42dd27c729e3e6984df463c
Florian Kaiser <florian.kaiser@hacking-lab.com>
2022-08-10 11:43:37 +0200

    🧪 Tested the first pricing plan

--- routes/pricing.go
+++ routes/pricing.go
@@ -10,8 +10,11 @@ func GetPricing(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	output, err := json.MarshalIndent(map[string]interface{}{
-		"plans": map[string]string{
-			"Coming": "soon",
+		"plans": map[string]interface{}{
+			"free": map[string]string{
+				"montly": "0 CHF",
+				"yearly": "0 CHF",
+			},
 		},
 	}, "", "\t")
 	if err == nil {
