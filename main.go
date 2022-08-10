b38d3cb5a17b7c22f939e06b56d46d68a0188ac9
Florian Kaiser <florian.kaiser@hacking-lab.com>
2022-08-10 09:45:49 +0200

    🔥 Added the `/info` route

--- main.go
+++ main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/Hacking-Lab/hl-cloud-api/routes"
 )
 
-func helloWorld(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(response, "This should work!")
-}
-
 func main() {
-	http.HandleFunc("/test", helloWorld)
+	http.HandleFunc("/info", routes.GetInfo)
 	http.ListenAndServe(":1337", nil)
 }
